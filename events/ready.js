b4286fceed0f31c9be5f32c90d4f7a20572d0290
kier2023 <kieronskelton.personal@gmail.com>
2024-04-07 14:25:21 +0100

    Presence & Console Logging

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,12 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        console.log(`Gekkō online. (${client.user.tag})`);
+        console.log(`✅ Logged in as ${client.user.username} (${client.user.id})`);
+        console.log(`✅ Latency is ${Math.round(client.ws.ping)}ms`);
+        console.log(`✅ ${client.user.username} (${client.user.id}) is ready to use!`);
+
+        client.user.setActivity({
+            name: "/gekko"
+        })
     }
-}
+};
