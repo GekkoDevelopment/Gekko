686ea1bc89d35b34147cf8758e02aadd92c2d3f6
RedMeansWar <dev.brandon.o@gmail.com>
2024-04-07 14:54:06 -0500

    chess definitions again

---
+++ models/chessPieces.js
@@ -0,0 +1,12 @@
+/*
+-------------------------------------- PIECES AND MOVEMENTS --------------------------------------
+Pawn - It may move one square directly forward, it may move two squares directly forward on its first move, and it may capture one square diagonally forward.
+Rook - The rook can move forward, backward or sideways, but cannot move diagonally.
+Knight - It either moves up or down one square vertically and over two squares horizontally OR up or down two squares vertically and over one square horizontally. This movement can be remembered as an "L-shape" because it looks like a capital "L".
+Bishop - The bishop moves diagonally in any direction it wishes and as far as it wishes as long as the squares are free. If an opposing piece blocks its way the bishop can capture it and occupy its square.
+King - the king can move one square in any direction, including horizontally, vertically, or diagonally. However, the king can only move one square at a time and cannot move into a square that is being attacked by an opponent's piece.
+Queen - The Queen can move 1-7 squares in any direction, up, down, left, right, or diagonal, until the Queen reaches an obstruction or captures a piece; however, the Queen cannot jump over pieces and can only capture one piece per turn.
+-------------------------------------- MOVE DEFINITIONS --------------------------------------
+Castling kingside (short castling) consists of moving the king to g1 and the rook to f1 for White, or moving the king to g8 and the rook to f8 for Black.
+Castling queenside (long castling) consists of moving the king to c1 and the rook to d1 for White, or moving the king to c8 and the rook to d8 for Black
+*/
