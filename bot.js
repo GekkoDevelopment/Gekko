cc14ea3c7d492d03318969f29f094854259d8941
kier2023 <kieronskelton.personal@gmail.com>
2024-04-07 17:49:06 +0100

    FIXED | Prefix Command

--- bot.js
+++ bot.js
@@ -15,127 +15,97 @@ const pterodactyl = new NodeactylClient(config.panel.host, config.panel.apiKey);
 ///// Prefix Commands ////
 
 client.on('messageCreate', async message => {
-	let prefix = "-d"
-	
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+    let prefix = "-d"
+    
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLocaleLowerCase();
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLocaleLowerCase();
 
-	if (command === 'bot-restart') {
-		if (!message.guild.id === '1226501941249576980') return;
+    if (command === 'bot-restart') {
+        if (message.guild.id !== '1226501941249576980') return;
 
-		message.channel.send('Gekkō is now restarting this will take a few seconds...');
-		let logChannel = client.guilds.cache.get('1226501941249576980').channels.cache.get('1226548220801450074');
+        message.channel.send('Gekkō is now restarting; this will take a few seconds...');
+        let logChannel = client.guilds.cache.get('1226501941249576980').channels.cache.get('1226548220801450074');
 
-		let logEmbed = new EmbedBuilder()
-		.setTitle('Gekkō Restart')
-		.setColor(`${color.bot}`)
-		.addFields
-		({
-			name: 'Restarter',
-			value: `${message.author}`,
-			inline: true
-		},
-		{
-			name: 'Restarter ID',
-			value: `${message.author.id}`,
-			inline: true
-		})
+        let logEmbed = new EmbedBuilder()
+            .setTitle('Gekkō Restart')
+            .setColor(`${color.bot}`)
+            .addFields(
+                { name: 'Restarter', value: `${message.author}`, inline: true },
+                { name: 'Restarter ID', value: `${message.author.id}`, inline: true }
+            );
 
-		let permissionErrorEmbed = new EmbedBuilder()
-		.setTitle('**Permissions Error: 50013**')
-		.addFields
-		({
-			name: 'Error Message:',
-			value: '```You lack permissions to perform that action```'
-		});
+        let permissionErrorEmbed = new EmbedBuilder()
+            .setTitle('**Permissions Error: 50013**')
+            .addFields(
+                { name: 'Error Message:', value: '```You lack permissions to perform that action```' }
+            );
 
-		if (!message.author.id === config.developer.dev1Id || !message.author.id === config.developer.dev2Id) {
-			message.channel.send({ embeds: [permissionErrorEmbed] });
-		}
+        if (message.author.id !== config.developer.dev1Id && message.author.id !== config.developer.dev2Id) {
+            message.channel.send({ embeds: [permissionErrorEmbed] });
+            return; // Exit if permission denied
+        }
 
-		setTimeout(function() {
-			pterodactyl.restartServer(config.panel.gekkoServerId);
-		}, 3000);
-		
-		logChannel.send({ embeds: [logEmbed] });
-	}
+        setTimeout(function() {
+            pterodactyl.restartServer(config.panel.gekkoServerId);
+        }, 3000);
+        
+        logChannel.send({ embeds: [logEmbed] });
+    }
 
-	if (command === 'bot-stats' && message.author === config.developer.dev1Id || message.author === config.developer.dev1Id) {
-		if (!message.guild.id === '1226501941249576980' && !message.channel.id === '') return;
+    if (command === 'bot-stats' && (message.author.id === config.developer.dev1Id || message.author.id === config.developer.dev2Id)) {
+        if (message.guild.id !== '1226501941249576980' || message.channel.id === '') return;
 
-		let logChannel = client.guilds.cache.get('1226501941249576980').channels.cache.get('1226548220801450074');
-		let totalSeconds = (client.uptime / 1000);
-		let days = Math.floor(totalSeconds / 86400);
-		
-		totalSeconds %= 86400;
+        let logChannel = client.guilds.cache.get('1226501941249576980').channels.cache.get('1226548220801450074');
+        let totalSeconds = (client.uptime / 1000);
+        let days = Math.floor(totalSeconds / 86400);
+        
+        totalSeconds %= 86400;
 
-		let hours = Math.floor(totalSeconds / 3600);
-		totalSeconds %= 3600;
-		let minutes = Math.floor(totalSeconds / 60);
-		let seconds = Math.floor(totalSeconds % 60);
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = Math.floor(totalSeconds % 60);
 
-		let uptime = `${days} days, ${hours} hours, ${minutes} minutes & ${seconds}`
-		let latency = `${client.ws.ping}ms`
+        let uptime = `${days} days, ${hours} hours, ${minutes} minutes & ${seconds} seconds`;
+        let latency = `${client.ws.ping}ms`;
 
-		let logEmbed = new EmbedBuilder()
-		.setTitle('Gekkō Stats Log')
-		.setColor(`${color.bot}`)
-		.setThumbnail(message.author.avatar)
-		.addFields
-		({
-			name: 'Requester',
-			value: `${message.author}`,
-			inline: true
-		},
-		{
-			name: 'Requester ID',
-			value: `${message.author.id}`,
-			inline: true
-		});
+        let logEmbed = new EmbedBuilder()
+            .setTitle('Gekkō Stats Log')
+            .setColor(`${color.bot}`)
+            .setThumbnail(message.author.displayAvatarURL())
+            .addFields(
+                { name: 'Requester', value: `${message.author}`, inline: true },
+                { name: 'Requester ID', value: `${message.author.id}`, inline: true }
+            );
 
-		let permissionErrorEmbed = new EmbedBuilder()
-		.setTitle('**Permissions Error: 50013**')
-		.setColor('Red')
-		.addFields
-		({
-			name: 'Error Message:',
-			value: '\`\`\`You lack permission to perform this action.\`\`\`'
-		});
+        let permissionErrorEmbed = new EmbedBuilder()
+            .setTitle('**Permissions Error: 50013**')
+            .setColor('Red')
+            .addFields(
+                { name: 'Error Message:', value: '```You lack permission to perform this action.```' }
+            );
 
-		let statEmbed = new EmbedBuilder()
-		.setTitle('Gekkō Bot Stats')
-		.setThumbnail(client.user.avatarURL)
-		.setColor(color.bot)
-		.addFields
-		({
-			name: 'Server Amount',
-			value: `${client.guilds.cache.size}`, inline: true
-		},
-		{
-			name: 'Latency',
-			value: `${latency}`,
-			inline: true
-		},
-		{
-			name: 'Client ID',
-			value: `${client.user.id}`,
-			inline: true
-		},
-		{
-			name: 'Uptime',
-			value: `\`\`\`${uptime}\`\`\``,
-			inline: false
-		})
-		
-		if (!message.author.id === config.developer.dev1Id || !message.author.id === config.developer.dev2Id) {
-			message.channel.send({ embeds: [permissionErrorEmbed] });
-		}
+        if (message.author.id !== config.developer.dev1Id && message.author.id !== config.developer.dev2Id) {
+            message.channel.send({ embeds: [permissionErrorEmbed] });
+            return; // Exit if permission denied
+        }
 
-		message.channel.send({ embeds: [statEmbed] });
-		logChannel.send({ embeds: [logEmbed] });
-	}
+        let statEmbed = new EmbedBuilder()
+            .setTitle('Gekkō Bot Stats')
+            .setThumbnail(client.user.displayAvatarURL())
+            .setColor(color.bot)
+            .addFields(
+                { name: 'Server Amount', value: `${client.guilds.cache.size}`, inline: true },
+                { name: 'Latency', value: `${latency}`, inline: true },
+                { name: 'Client ID', value: `${client.user.id}`, inline: true },
+                { name: 'Uptime', value: `\`\`\`${uptime}\`\`\``, inline: false }
+            );
+
+        message.channel.send({ embeds: [statEmbed] });
+        logChannel.send({ embeds: [logEmbed] });
+    }
 });
 
 //////////////////////////////////
