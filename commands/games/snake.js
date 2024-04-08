1ae47cceea3c2b286cc1da4f189a427b5795ab20
kier2023 <kieronskelton.personal@gmail.com>
2024-04-08 01:34:39 +0100

    Snake v1

---
+++ commands/games/snake.js
@@ -0,0 +1,41 @@
+const { Snake } = require('discord-gamecord')
+const { SlashCommandBuilder } = require('discord.js')
+const { color } = require('../../config.js')
+const config = require('../../config.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('snake')
+    .setDescription('Start a game of Classic Snake'),
+    async execute(interaction) {
+        const Game = new Snake({
+            message: interaction,
+            isSlashGame: false,
+            embed: {
+                title: 'Snake Game',
+                overTitle: 'Game Over',
+                color: '#7B598D'
+            },
+            emojis: {
+                board: '⬛',
+                food: '🍎',
+                up: '⬆️',
+                down: '⬇️',
+                left: '⬅️',
+                right: '➡️',
+            },
+            stopButton: 'Stop',
+            timoutTime: 60000,
+            snake: { head: '🟢', body: '🟩', tail: '🟢', over: '☠️'},
+            foods: ['🍎', '🍇', '🍊', '🫐', '🥕', '🥝', '🌽'],
+            playerOnlyMessage: 'Only {player} can use these buttons.'
+        });
+
+        Game.startGame();
+        Game.on('gameOver', result => {
+            return;
+
+        });
+
+    }
+}
