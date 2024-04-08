ed62a77818893bdeff98be2ec9feccc03e8a0b43
kier2023 <kieronskelton.personal@gmail.com>
2024-04-08 19:03:50 +0100

    gif.js

--- commands/fun/gif.js
+++ commands/fun/gif.js
@@ -1,10 +1,34 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const superagent = require('superagent');
+const config = require('../../config');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('gif').setDescription('Get a random GIF.')
+        .setName('gif')
+        .setDescription('Get a random GIF.')
         .addStringOption(option => option.setName('term').setDescription('The search term you want your meme to have.').setRequired(true)),
     async execute(interaction) {
-        
+        await interaction.deferReply({ ephemeral: false });
+        const { options } = interaction;
+        const query = options.getString('term');
+        const apiKey = config.apiKeys.tenorApi;
+        const clientKey = "Gekko";
+        const lmt = 8;
+
+        let choice = Math.floor(Math.random() * lmt);
+
+        const link = `https://tenor.googleapis.com/v2/search?q=${query}&key=${apiKey}&client_key=${clientKey}&limit=${lmt}`;
+
+        try {
+            const response = await superagent.get(link);
+            if (response && response.body && response.body.results && response.body.results.length > 0) {
+                await interaction.editReply({ content: response.body.results[choice].itemurl });
+            } else {
+                throw new Error("No results found");
+            }
+        } catch (error) {
+            console.error(error);
+            await interaction.editReply({ content: `Error: I couldn't find a matching gif to \`${query}\`!` });
+        }
     }
-}
+};
