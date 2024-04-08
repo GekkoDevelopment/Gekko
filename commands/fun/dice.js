5644b2f6e6b128f1009b89c2097fb5d0a13fe47d
kier2023 <kieronskelton.personal@gmail.com>
2024-04-08 21:53:44 +0100

    Dice

--- commands/fun/dice.js
+++ commands/fun/dice.js
@@ -1,9 +1,19 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+const colors = require('../../models/colors');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('dice').setDescription('Roll the dice.'),
     async execute(interaction) {
+
+        const dice = Math.floor(Math.random() * 6) + 1 -1 + 1;
+        const diceEmbed = new EmbedBuilder()
+        .setAuthor({ name: `${interaction.user.tag}` })
+        .setColor(colors.bot)
+        .setTimestamp()
+        .setDescription(`**You rolled a ${dice}** :game_die:`)
+        .setFooter({ text: `${interaction.client.user.username}`, iconUR: `${interaction.client.displayAvatarURL}` });
         
+        await interaction.reply({ embeds: [diceEmbed] });
     }
 }
