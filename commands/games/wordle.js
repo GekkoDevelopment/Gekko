084728929bf3939daced2045ec779a0004d7e8e5
kier2023 <kieronskelton.personal@gmail.com>
2024-04-09 22:53:20 +0100

    Wordle

---
+++ commands/games/wordle.js
@@ -0,0 +1,28 @@
+const { Wordle } = require('discord-gamecord');
+const { SlashCommandBuilder } = require('discord.js');
+const colors = require('../../models/colors');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('wordle')
+    .setDescription('Start a game of Wordle'),
+    async execute(interaction) {
+        const Game = new Wordle({
+            message: interaction,
+            isSlashGame: false,
+            embed: {
+              title: 'Wordle',
+              color: colors.bot,
+            },
+            customWord: null,
+            timeoutTime: 60000,
+            winMessage: 'You won! The word was **{word}**.',
+            loseMessage: 'You lost! The word was **{word}**.',
+            playerOnlyMessage: 'Only {player} can use these buttons.'
+          });
+          
+          Game.startGame();
+          Game.on('gameOver', result => {
+          });
+    }
+}
