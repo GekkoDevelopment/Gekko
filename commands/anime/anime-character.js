7055f1389653a0acb12c89d143e7249d76c2d01c
kier2023 <kieronskelton.personal@gmail.com>
2024-04-12 00:59:31 +0100

    Push before bed

--- commands/anime/anime-character.js
+++ commands/anime/anime-character.js
@@ -1,19 +1,64 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const config = require('../../config.js');
-const db = require('mysql');
-
-let mysql = db.createConnection({
-    host: config.database.host,
-    user: config.database.username,
-    password: config.database.password
-});
+const fetch = require('node-fetch');
+const colors = require('../../models/colors');
+const { emojis } = require('../../config');
+const animeInfo = require('./anime-info');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('anime-character').setDescription('Get information about an anime character.')
-        .addStringOption(option => option.setName('character').setDescription('The anime character to get information about.').setRequired(true)),
+        .setName('anime-character')
+        .setDescription('Look up information about a specific anime character.')
+        .addStringOption(option => option.setName('character').setDescription('The anime character to get information from.').setRequired(true)),
     async execute(interaction) {
-        const animeCharacter = interaction.options.getString('character');
-        
-    }
-}
+        try {
+            const characterName = interaction.options.getString('character');
+            let option = {
+                url: `https://kitsu.io/api/edge/characters?filter[name]=${characterName}`,
+                method: `GET`,
+                headers: {
+                    'Content-Type': "application/vnd.api+json",
+                    'Accept': "application/vnd.api+json",
+                }
+            };
+            
+            const response = await fetch(option.url, option);
+            const data = await response.json();
+            console.log(data)
+
+            if (data.data && data.data.length > 0) {
+                const characterInfo = data.data[0]; 
+                const description = characterInfo.attributes.description || 'No description available.';
+                const about = characterInfo.attributes.about || 'No information available.';
+            
+                const truncatedDescription = description.length > 300 ? description.slice(0, 300) + '...' : description;
+            
+                const embed = new EmbedBuilder()
+                    .setTitle(`${characterInfo.attributes.canonicalName}`)
+                    .setDescription(`${truncatedDescription}[[View More]](https://kitsu.io/characters/${characterInfo.id})`)
+                    .addFields(
+                        {
+                            name: 'Anime',
+                            value: about
+                        }
+                    )
+                    .setColor(colors.bot)
+                    .setImage(characterInfo.attributes.image.original);
+                
+                await interaction.reply({ embeds: [embed] });
+            } else {
+                const errorEmbed = new EmbedBuilder()
+                    .setTitle('Search Error:')
+                    .setColor('RED')
+                    .setDescription('No character found with that name.')
+                await interaction.reply({ embeds: [errorEmbed] });
+            }
+        } catch(error) {
+            const catchErrorEmbed = new EmbedBuilder()
+                console.log(error)
+                .setTitle('Unexpected Error:')
+                .setDescription(`\`\`\`\n${error}\`\`\`\n\nReport this to a developer at our [Discord Server](https://discord.gg/7E5eKtm3YN)`)
+                .setColor('Red')
+            await interaction.reply({ embeds: [catchErrorEmbed] });
+        }
+    },
+};
