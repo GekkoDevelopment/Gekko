334133aa3a8b97fdcdeb9fad239e76ca7dceed5c
Brandon O <122373753+RedMeansWar@users.noreply.github.com>
2024-04-11 17:05:44 -0500

    todo: fix allow nsfw command

---
+++ commands/anime/allow-nsfw-anime.js
@@ -0,0 +1,69 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelFlags, ChannelType, MediaChannel, TextChannel, PermissionFlagsBits, PermissionsBitField } = require('discord.js');
+const db = require('mysql');
+const config = require('../../config.js');
+const colors = require('../../models/colors');
+
+const mysql = db.createConnection({
+    
+})
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('allow-nsfw-anime').setDescription('This feature is off by default. If you want NSFW anime stuff on turn it on using this command.'),
+    async execute(interaction) {
+        
+        const confirmButton1 = new ButtonBuilder().setLabel('Yes').setCustomId('confirm_nsfw_1').setStyle(ButtonStyle.Success);
+        const denyButton1 = new ButtonBuilder().setLabel('No').setCustomId('deny_nsfw_1').setStyle(ButtonStyle.Danger);
+
+        const confirmButton2 = new ButtonBuilder().setLabel('Yes').setCustomId('confirm_nsfw_2').setStyle(ButtonStyle.Success);
+        const denyButton2 = new ButtonBuilder().setLabel('No').setCustomId('deny_nsfw_2').setStyle(ButtonStyle.Danger);
+        
+        const embedConfirmEmbed1 = new EmbedBuilder()
+        .setTitle('Enable Not Safe For Work Feature')
+        .setColor(colors.deepPink)
+        .setDescription("This command turns on NSFW anime features (such as the waifu command will show NSFW images)." + 
+        "This command/feature is turned off by default... **Are you sure you want to enable this feature?**");
+
+        const embedConfirmEmbed2 = new EmbedBuilder()
+        .setTitle('Enable Not Safe For Work Feature')
+        .setColor(colors.deepPink)
+        .setDescription("**Are you sure you're sure?**");
+
+        const actionRow1 = new ActionRowBuilder().addComponents(confirmButton1, denyButton1);
+        const actionRow2 = new ActionRowBuilder().addComponents(confirmButton2, denyButton2);
+
+        let message = interaction.fetchReply();
+
+        if (!message.channel.nsfw && !interaction.member.permissions.has(PermissionFlagsBits.Administrator)) {
+            const permissionErrorEmbed = new EmbedBuilder()
+            .setTitle('Permissions Error: 50013')
+            .addFields(
+                {
+                    name: 'Error Message:',
+                    value: '```\nYou lack permissions to perform that action, and you are not in a NSFW channel.```',
+                    inline: true
+                }
+            )
+            .setColor('Red');
+
+            return await interaction.reply({ embeds: [permissionErrorEmbed], ephemeral: true });
+        }
+
+        if (message.channel.nsfw && !interaction.member.permissions.has(PermissionFlagsBits.Administrator)) {
+            const permissionErrorEmbed = new EmbedBuilder()
+            .setTitle('Permissions Error: 50013')
+            .addFields(
+                {
+                    name: 'Error Message:',
+                    value: '```\nYou lack permissions to perform that action, and you are not in a NSFW channel.```',
+                    inline: true
+                }
+            )
+            .setColor('Red');
+            
+            return await interaction.reply({ embeds: [permissionErrorEmbed], ephemeral: true });
+        }
+
+        
+    }
+}
--- models/quotes.js
+++ models/quotes.js
@@ -1,3 +1,5 @@
 module.exports = {
-    
+    DisguisedToOurselves: "We are so accustomed to disguise ourselves to others, that in the end, we become disguised to ourselves.",
+    ResistingPassionWeaknessOurStrength: "If we resist our passions, it is more due to their weakness than our strength.",
+    OldPeoplGiveGoodAdvice: "Old people love to give good advice; it compensates them for their inability to set a bad example."
 }
