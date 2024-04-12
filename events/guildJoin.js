532edaf944accc77abbbe1fba2f119ea9a89888e
kier2023 <kieronskelton.personal@gmail.com>
2024-04-12 22:09:44 +0100

    FIXED | Guild Join

--- events/guildJoin.js
+++ events/guildJoin.js
@@ -11,12 +11,7 @@ module.exports = {
             const imageUrl = await MySQL.getColumnValuesWithGuildId(guildId, 'image_url');
             const welcomeChannelId = await MySQL.getColumnValuesWithGuildId(guildId, 'welcome_channel_id');
 
-            console.log("Welcome Message:", welcomeMessage);
-            console.log("Image URL:", imageUrl);
-            console.log("Welcome Channel ID:", welcomeChannelId);
-
             const welcomeChannel = member.guild.channels.cache.get(welcomeChannelId);
-            console.log('Welcome Channel:', welcomeChannel)
 
             if (!welcomeChannel) {
                 console.log("Welcome channel not found.", welcomeChannelId);
