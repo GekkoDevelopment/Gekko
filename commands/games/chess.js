634b4926c53d171e5a4da65a08ffee233f20a90a
kier2023 <kieronskelton.personal@gmail.com>
2024-04-16 20:28:18 +0100

    Maybe use API? Only installed package was node-fetch

--- commands/games/chess.js
+++ commands/games/chess.js
@@ -10,29 +10,8 @@ module.exports = {
             const playerA = interaction.user;
             const playerB = interaction.options.getUser('user');
 
-            const Lichess = await fetch('https://lichess.org/api/challenge', {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Bearer lip_DMx26QizmDMq5YcCPPVD`,
-                    'Content-Type': 'application/json'
-                },
-
-                body: JSON.stringify({
-                    rated: false,
-                    clock: { limit: 300, increment: 0 },
-                    color: 'random',
-                    variant: 'standard',
-                    challenges: { user: [playerB.id] }
-                })
-            });
-
-            if (!lichessResponse.ok) {
-                return interaction.reply('Cant create game for some reason')
-            }
-
-            
-
+            const Lichess = await fetch('https://lichess.org/api/challenge'
         }
 };
 
-// lip_DMx26QizmDMq5YcCPPVD
+// lip_DMx26QizmDMq5YcCPPVD = API KEY ??? 
