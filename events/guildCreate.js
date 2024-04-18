b421e5bf6da9f0c34878114106d981299aca51e6
kier2023 <kieronskelton.personal@gmail.com>
2024-04-18 19:18:50 +0100

    GuildCreate logging

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,6 @@
 const { Events, EmbedBuilder, AttachmentBuilder } = require('discord.js');
 const MySQL = require('../models/mysql.js');
+const config = require('../config.js');
 
 module.exports = {
     name: Events.GuildCreate,
@@ -19,6 +20,51 @@ module.exports = {
                 MySQL.insertIntoGuildTable(columns, values);
             }
         });
+
+        const owner = await guild.fetchOwner();
+        const ownerTag = owner ? owner.user.tag : 'Unknown Owner';
+
+        const guildCreateEmbed = new EmbedBuilder()
+            .setTitle('I just joined another guild!')
+            .setColor('Green')
+            .addFields(
+                {
+                    name: 'Guild Name',
+                    value: `\`${guild.name}\``,
+                    inline: true
+                },
+                {
+                    name: 'Guild ID:',
+                    value: `\`${guild.id}\``,
+                    inline: true
+                },
+                {
+                    name: 'Owner',
+                    value: `\`${ownerTag}\``,
+                    inline:true
+                },
+                {
+                    name: 'Member Count',
+                    value: `\`${guild.memberCount}\``,
+                    inline: true
+                },
+                {
+                    name: 'Boost Level',
+                    value: `\`${guild.premiumTier}\``,
+                    inline: true
+                },
+                {
+                    name: 'Region',
+                    value: `\`${guild.region}\``,
+                    inline: true
+                }
+            )
+            .setTimestamp();
+
+        const client = guild.client
+        const loggingChannelId = config.developer.devTestLogChannel
+        const loggingChannel = client.channels.cache.get(loggingChannelId)
+        await loggingChannel.send({ embeds: [guildCreateEmbed] })
 
     }
 }
