c9a54a9cd555a0aa5205c481179b76c247a18eb6
kier2023 <kieronskelton.personal@gmail.com>
2024-04-18 20:36:47 +0100

    lockdown config

--- commands/admin/set-lockdownChannels.js
+++ commands/admin/set-lockdownChannels.js
@@ -8,8 +8,12 @@ module.exports = {
         .addStringOption(option => option.setName('channels').setDescription('Enter the channels (seperated by commas)').setRequired(true)),
     
     async execute(interaction) {
-        const channels = interaction.options.getString('channels')
-        console.log(channels)
+        const channelsInput = interaction.options.getString('channels')
+        const channelIds = channelsInput.match(/\d+/g);
+        console.log(channelIds)
+
+        await MySQL.updateColumnValue('lockdown_config', 'guild_id', interaction.guild.id)
+        await MySQL.updateColumnValue('lockdown_config', 'channel_id', channelIds)
 
         await interaction.reply({ content: 'done.', ephemeral: true })
     }
