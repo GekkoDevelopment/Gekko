ca1381697527e0e10546c83fbcc33e7a3c663a1a
RedMeansWar <dev.brandon.o@gmail.com>
2024-04-18 12:43:32 -0500

    cleaned up

--- events/roleAssignment.js
+++ events/roleAssignment.js
@@ -6,26 +6,14 @@ module.exports = {
     name: Events.GuildMemberAdd,
     async execute(member) {
         try {
-            const joinRole = await MySQL.getColumnValuesWithGuildId(member.guild.id, 'join_role');
-            
-            if (!joinRole) {
-                console.log('No join role found for the guild');
-                return;
+            const joinRoleId = await MySQL.getColumnValuesWithGuildId(member.guild.id, 'join_role')
+
+            if (joinRoleId) {
+                const joinRole = member.guild.roles.cache.get(joinRoleId)
+                await member.roles.add(joinRole)
+            } else {
+                return; // Do nothing
             }
-
-<<<<<<< HEAD
-            const role = await member.guild.roles.fetch(joinRole.toString());
-=======
-        const joinRoleId = await MySQL.getColumnValuesWithGuildId(member.guild.id, 'join_role')
-        if (joinRoleId) {
-            console.log(joinRoleId)
-
-            const joinRole = member.guild.roles.cache.get(joinRoleId)
-            await member.roles.add(joinRole)
-        } else {
-            return; // Do nothing
-        }
->>>>>>> 280c6810a6c8fc913addad0df48208567330bde5
 
             member.roles.add(role);
             
