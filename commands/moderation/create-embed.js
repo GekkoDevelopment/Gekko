fd7f62fde67ac3ef34085916b2596d3eb9a57352
Brandon O <122373753+RedMeansWar@users.noreply.github.com>
2024-04-19 08:14:04 -0500

    just a push so I can get on my main pc

--- commands/moderation/create-embed.js
+++ commands/moderation/create-embed.js
@@ -10,7 +10,12 @@ let mysql = db.createConnection({
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('create-embed').setDescription('Create a custom embed!')
-        .addStringOption(option => option.setName('title').setDescription('Set the title of the custom embed.')),
+        .addStringOption(option => option.setName('title').setDescription('Set the title of the custom embed.').setRequired(true))
+        .addStringOption(option => option.setName('description').setDescription('Set the description of the embed.'))
+        .addStringOption(option => option.setName('color').setDescription('The color of the embed. (you can use hex code too!)'))
+        .addStringOption(option => option.setName('footer-text').setDescription('The text of the footer.'))
+        .addStringOption(option => option.setName('footer-image-url').setDescription('The image url for the footer.'))
+        .addStringOption(option => option.setName('thumbnail-url').setDescription('The thumbnail for the embed.')),
     async execute(interaction) {
 
     }
