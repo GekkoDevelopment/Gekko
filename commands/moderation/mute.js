9ff41d71eb31f10e25d3a996635f2d52583b2628
RedMeansWar <dev.brandon.o@gmail.com>
2024-04-19 10:54:46 -0500

    more philosophy quotes

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,41 +1,15 @@
 const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
-const db = require('mysql');
 const config = require('../../config.js');
 const color = require('../../models/colors.js');
-
-const mysql = db.createConnection({
-    host: config.database.host,
-    database: config.database.name,
-    user: config.database.username,
-    password: config.database.password,
-    port: config.database.port
-})
-
-mysql.query(`
-    CREATE TABLE IF NOT EXISTS muted_users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        guild_id VARCHAR(255) NOT NULL,
-        muted_role_id VARCHAR(255) NOT NULL,
-        muted_user_id VARCHAR(255) NOT NULL
-    )
-`);
-
-/* EVERYTHING I CHANGED:
-- interaction.reply() inside the mysql callback was not needed.
-- added a check to make sure the muted role existed in the db.
-- added embed error handling to tidy up the errors.
-- I'm not to certain around mysql queries, so I prolly made some mistakes around them, but you can double check. 
-
-It will say Gekko is thinking forever until logic for handling when muted role exists has been added. 
-Other than that, I think I resolved all the errors?? ^_-
-*/
+const MySQL = require('../../models/mysql.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('mute').setDescription('Mute a user from your discord server.')
         .addUserOption(option => option.setName('user').setDescription('The user you want to mute.').setRequired(true)),
     async execute(interaction) {
-
+        await interaction.deferReply();
+        
         if (!interaction.member.permissions.has(PermissionFlagsBits.MuteMembers)) {
             const permissionErrorEmbed = new EmbedBuilder()
             .setTitle('Permissions Error: 50013')
@@ -55,111 +29,11 @@ module.exports = {
         const mutedUser = interaction.options.getUser('user');
         const guildId = interaction.guild.id;
 
-        mysql.query(
-            'SELECT muted_role_id FROM muted_users WHERE guild_id = ?',
-            [guildId],
-            (err, rows) => {
-                if (err) {
-                    console.error(`Error checked muted role for ${guildId}:`, err);
-                    
-                    const stackLines = err.stack.split('\n');
-                    const relevantLine = stackLines[1];
-                    const errorMessage = relevantLine.replace(/^\s+at\s+/g, '')
-                    const errorDescription = err.message;
-    
-                    const catchErrorEmbed = new EmbedBuilder()
-                    .setTitle('Unexpected Error:')
-                    .setDescription(`\`\`\`\n${errorMessage} \n\n${errorDescription}\`\`\`\n\nReport this to a developer at our [Discord Server](https://discord.gg/7E5eKtm3YN)`)
-                    .setColor('Red')
-                    .setTimestamp()
-                    .setFooter({ text: 'Gekkō Development', iconURL: interaction.client.user.displayAvatarURL() });
-                   return interaction.reply({ embeds: [catchErrorEmbed], ephemeral: true });
-                }
+        MySQL.insertValueIfNotExists('muted_users', 'guild_id', guildId);
+        const mutedRoleId = MySQL.getValueFromTableWithCondition('muted_users', 'muted_role_id', 'guild_id', guildId);
 
-                if (rows.length > 0 && rows[0].muted_role_id) {
-                        // put what ever you logic is for this??? :D :D 
-                } else {
-                    let guild = interaction.guild;
-
-                    if (!guild) {
-                        console.error(`Guild (${guild}) couldn't be found.`);
-                        const errorEmbed = new EmbedBuilder()
-                            .setTitle('Database Error:')
-                            .setDescription(`Guild couldn't be found`)
-                            .setColor('Red')
-                            .setTimestamp()
-                            .setFooter({ text: 'Gekkō Development', iconURL: interaction.client.user.displayAvatarURL() });
-                        return interaction.reply({ embeds: [errorEmbed] });
-                    }
-
-                    guild.roles.create({
-                        data: {
-                            name: 'Muted',
-                            permissions: [
-                                {
-                                    id: guildId,
-                                    type: 'ROLE',
-                                    permission: false,
-                                    deny: ['SEND_MESSAGES']
-                                },
-                                {
-                                    id: guildId,
-                                    type: 'SPEAK',
-                                    permission: false,
-                                    deny: ['SPEAK']
-                                }
-                            ]
-                        }
-                    })
-                    .then((role) => {
-                        mysql.query(
-                            'INSERT INTO muted_users (guild_id, muted_role_id) VALUES (?, ?)',
-                            [guildId, role.id],
-                            (err, result) => {
-                                if (err) {
-                                    console.error('Error saving muted role ID:', err);
-                                    const errorEmbed = new EmbedBuilder()
-                                        .setTitle('Database Error:')
-                                        .setDescription(`Failed to save muted role ID`)
-                                        .setColor('Red');
-                                    return interaction.reply({ embeds: [errorEmbed] });
-                                }
-                        
-                                const memberToMute = guild.members.cache.get(mutedUser.id);
-                        
-                                if (!memberToMute) {
-                                    return interaction.reply('Member not found!');
-                                }
-                        
-                                memberToMute.roles.add(role)
-                                .then(() => {
-                                    interaction.reply(`Successfully muted ${mutedUser}`);
-                                })
-                                .catch(err =>{ 
-                                    console.error('Error assigning muted role to member:', err);
-                                    const errorEmbed = new EmbedBuilder()
-                                        .setTitle('Mute Error:')
-                                        .setDescription(`Failed to mute user`)
-                                        .setColor('Red')
-                                        .setTimestamp()
-                                        .setFooter({ text: 'Gekkō Development', iconURL: interaction.client.user.displayAvatarURL() });
-                                    return interaction.reply({ embeds: [errorEmbed] });
-                                });
-                            }
-                        );
-                    }).catch((err) => {
-                        console.error(`Error created muted role for guild (${guildId}):`, err);
-                        const errorEmbed = new EmbedBuilder()
-                            .setTitle('Role Creation Error:')
-                            .setDescription(`Failed to create muted role`)
-                            .setColor('Red')
-                            .setTimestamp()
-                            .setFooter({ text: 'Gekkō Development', iconURL: interaction.client.user.displayAvatarURL() });
-                        return interaction.reply({ embeds: [errorEmbed] });
-                    });
-                }
-            }
-        );
-        interaction.deferReply({ ephemeral: true });
-    }
+        if (mutedRoleId === 'null' || mutedRoleId === 'undefined') {
+            
+        }
+    }  
 };
