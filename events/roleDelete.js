cb7b2ab88a03dfb88e64b27b0da0a902949f017d
kier2023 <kieronskelton.personal@gmail.com>
2024-04-24 16:00:06 +0100

    feat: role creation/deletion/update audit logging

---
+++ events/roleDelete.js
@@ -0,0 +1,41 @@
+const { Events, EmbedBuilder, AuditLogEvent } = require('discord.js');
+const config = require('../config');
+const MySQL = require('../models/mysql');
+
+module.exports = {
+    name: Events.GuildRoleDelete,
+    async execute(role) {
+        const logChannelId = await MySQL.getValueFromTableWithCondition('guilds', 'logging_channel', 'guild_id', role.guild.id);
+        
+        if (!logChannelId) {
+            return;
+        }
+
+        const logChannel = role.guild.channels.cache.get(logChannelId);
+        if (!logChannel) {
+            console.log(`${logChannelId} not found.`);
+            return;
+        }
+
+        const fetchedLogs = await role.guild.fetchAuditLogs({
+            type: AuditLogEvent.RoleDelete,
+            limit: 1
+        });
+
+        const firstEntry = fetchedLogs.entries.first();
+        if (!firstEntry || !firstEntry.executor) {
+            return;
+        }
+
+        const executor = firstEntry.executor;
+
+        const embed = new EmbedBuilder()
+            .setTitle(`${config.emojis.warning} Role Deleted`)
+            .setDescription(`> **role Name:** ${role.name} \n> **role ID:** ${role.id} \n> **Deleted by:** <@${executor.id}>`)
+            .setColor('Red')
+            .setTimestamp()
+            .setFooter({ text: 'Gekkō', iconURL: role.client.user.displayAvatarURL() });
+
+        logChannel.send({ embeds: [embed] });
+    }
+};
---
+++ events/roleUpdate.js
@@ -0,0 +1,54 @@
+const { Events, EmbedBuilder, AuditLogEvent } = require('discord.js');
+const config = require('../config');
+const MySQL = require('../models/mysql');
+
+module.exports = {
+    name: Events.GuildRoleUpdate,
+    async execute(oldRole, newRole) {
+        const logChannelId = await MySQL.getValueFromTableWithCondition('guilds', 'logging_channel', 'guild_id', newRole.guild.id);
+        
+        if (!logChannelId) {
+            return;
+        }
+
+        const logChannel = newRole.guild.channels.cache.get(logChannelId);
+        if (!logChannel) {
+            console.log(`${logChannelId} not found.`);
+            return;
+        }
+
+        const fetchedLogs = await newRole.guild.fetchAuditLogs({
+            type: AuditLogEvent.RoleUpdate,
+            limit: 1
+        });
+
+        const firstEntry = fetchedLogs.entries.first();
+        if (!firstEntry || !firstEntry.executor) {
+            return;
+        }
+
+        const executor = firstEntry.executor;
+
+        if (oldRole.name !== newRole.name) {
+            const embed = new EmbedBuilder()
+            embed.addFields(
+                {
+                    name: 'Previous Name:',
+                    value: `${oldRole.name}`,
+                    inline: true
+                },
+                {
+                    name: 'New Name:',
+                    value: `${newRole.name}`,
+                    inline:true
+                }
+            )
+            .setTitle(`${config.emojis.warning} Role Name Updated`)
+            .setDescription(`> **role Name:** ${newRole.name} \n> **role ID:** ${newRole.id} \n> **Role Mention:** <@&${newRole.id}> \n> **Deleted by:** <@${executor.id}>`)
+            .setColor('Orange')
+            .setTimestamp()
+            .setFooter({ text: 'Gekkō', iconURL: newRole.client.user.displayAvatarURL() });
+            logChannel.send({ embeds: [embed] })
+        }
+    }
+};
