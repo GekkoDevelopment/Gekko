9e3884d44ebb5a6405d00d13397dbff00c6d4558
kier2023 <kieronskelton.personal@gmail.com>
2024-05-06 16:20:16 +0100

    chore: embed testing

---
+++ embeds/gekko defaults/guildRestricted.js
@@ -0,0 +1,18 @@
+const { EmbedBuilder } = require('discord.js');
+const { emojis } = require('../../config');
+
+module.exports = 
+    {
+        embed: ( interaction, data ) =>  new EmbedBuilder()
+        .setTitle('Permissions Error: 50105')
+        .addFields(
+            {
+                name: 'Error Message:',
+                value: '```\nYour guild has been banned by Gekkō Development. If you feel like this is an error please contact the development team by joining our [Support Discord.](https://discord.gg/2aw45ajSw2)```',
+                inline: true
+            }
+        )
+        .setColor('Red')
+        .setTimestamp()
+        .setFooter({ text: 'Gekkō Development', iconURL: interaction.client.user.displayAvatarURL() })
+    }
--- interactions/commands/tickets/ticket-setup.js
+++ interactions/commands/tickets/ticket-setup.js
@@ -21,18 +21,7 @@ module.exports = {
         const restricted = MySQL.getValueFromTableWithCondition('guilds', 'restricted_guild', 'guild_id', interaction.guild.id);
 
         if (restricted === 'true') {
-            const permissionErrorEmbed = new EmbedBuilder()
-            .setTitle('Permissions Error: 50105')
-            .addFields(
-                {
-                    name: 'Error Message:',
-                    value: '```\nYour guild has been banned by Gekkō Development. If you feel like this is an error please contact the development team by joining our [Support Discord.](https://discord.gg/2aw45ajSw2)```',
-                    inline: true
-                }
-            )
-            .setColor('Red')
-            .setTimestamp()
-            .setFooter({ text: 'Gekkō Development', iconURL: interaction.client.user.displayAvatarURL() });
+            const permissionErrorEmbed = embeds.get('guildRestricted') (interaction);
             return await interaction.reply({ embeds: [permissionErrorEmbed], ephemeral: true });
         }
 
