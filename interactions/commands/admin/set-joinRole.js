fc00ea3331051d715cfd2f9e05a09629e2a7a23c
kier2023 <kieronskelton.personal@gmail.com>
2024-05-06 21:31:55 +0100

    refactor: error handling embeds

--- interactions/commands/admin/set-joinRole.js
+++ interactions/commands/admin/set-joinRole.js
@@ -11,49 +11,17 @@ module.exports = {
         const restricted = MySQL.getValueFromTableWithCondition('guilds', 'restricted_guild', 'guild_id', interaction.guild.id);
 
         if (restricted === 'true') {
-            const permissionErrorEmbed = new EmbedBuilder()
-            .setTitle('Permissions Error: 50105')
-            .addFields(
-                {
-                    name: 'Error Message:',
-                    value: '```\nYour guild has been banned by Gekkō Development. If you feel like this is an error please contact the development team by joining our [Support Discord.](https://discord.gg/2aw45ajSw2)```',
-                    inline: true
-                }
-            )
-            .setColor('Red')
-            .setTimestamp()
-            .setFooter({ text: 'Gekkō Development', iconURL: interaction.client.user.displayAvatarURL() });
+            const permissionErrorEmbed = embeds.get('guildRestricted')(interaction);
             return await interaction.reply({ embeds: [permissionErrorEmbed], ephemeral: true });
         }
         
         if (!interaction.member.permissions.has(PermissionFlagsBits.ManageGuild)) {
-            const permissionErrorEmbed = new EmbedBuilder()
-            .setTitle(`${emojis.warning} Permissions Error: 50013`)
-            .addFields(
-                {
-                    name: 'Error Message:',
-                    value: '```\nYou need the MANAGE_GUILD permission to use this command.```',
-                    inline: true
-                }
-            )
-            .setColor('Red')
-            .setTimestamp()
-            .setFooter({ text: 'Gekkō Development', iconURL: interaction.client.user.displayAvatarURL() });
+            const permissionErrorEmbed = embeds.get('permissionsError')(interaction);
             return await interaction.reply({ embeds: [permissionErrorEmbed], ephemeral: true });
         }
 
         if (!interaction.guild.members.me.permissions.has(PermissionFlagsBits.ManageRoles)) {
-            const permissionErrorEmbed = new EmbedBuilder()
-            .setTitle(`${emojis.warning} Permissions Error: 50013`)
-            .addFields(
-                {
-                    name: 'Error Message:',
-                    value: '```\nI need the MANAGE_ROLES permission to use this command.```',
-                }
-            )
-            .setColor('Red')
-            .setTimestamp()
-            .setFooter({ text: 'Gekkō Development', iconURL: interaction.client.user.displayAvatarURL() });
+            const permissionErrorEmbed = embeds.get('botPermissionsError');
             return await interaction.reply({ embeds: [permissionErrorEmbed], ephemeral: true })
         }
 
--- interactions/commands/anime/neko.js
+++ interactions/commands/anime/neko.js
@@ -19,18 +19,7 @@ module.exports = {
         if (restricted === 'true') {
             await interaction.deferReply();
             
-            const permissionErrorEmbed = new EmbedBuilder()
-            .setTitle('Permissions Error: 50105')
-            .addFields(
-                {
-                    name: 'Error Message:',
-                    value: '```\nYour guild has been banned by Gekkō Development. If you feel like this is an error please contact the development team by joining our [Support Discord.](https://discord.gg/2aw45ajSw2)```',
-                    inline: true
-                }
-            )
-            .setColor('Red')
-            .setTimestamp()
-            .setFooter({ text: 'Gekkō Development', iconURL: interaction.client.user.displayAvatarURL() });
+            const permissionErrorEmbed = embeds.get('guildRestricted')(interaction);
             return await interaction.editReply({ embeds: [permissionErrorEmbed], ephemeral: true });
         }
 
