8bc9060167cb6d09ac770ef09f866dab7d29a736
kier2023 <kieronskelton.personal@gmail.com>
2024-05-16 19:58:35 +0100

    exchange cmd

---
+++ interactions/commands/economy/exchange.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const MySQL = require("../../../models/mysql");
+const colors = require("../../../models/colors");
+const config = require("../../../config");
+const { execute } = require("./transfer");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('hearts-exchange').setDescription('Exchange Gekkō Hearts for Bills'),
+    async execute(interaction) {
+        const currentHeartsBal = await MySQL.getValueFromTableWithCondition('economy', 'cash_amount', 'user_id', interaction.user.id);
+        if (currentHeartsBal === null || currentHeartsBal < 100) {
+            const balanceError = new EmbedBuilder()
+            .setTitle(`${config.emojis.noted} There seems to be a transaction error...`)
+            .setDescription(`${interaction.user}, after looking through your finances, you don't have enough to complete this transaction! \n\n***Here's a breakdown of your accounts:***`)
+            .setColor(colors.bot)
+            .addFields(
+                {
+                    name: '**Current Balance**',
+                    value: `${config.emojis.gekkoCoin} \`0\``,
+                },
+                {
+                    name: '**What you need**',
+                    value: `${config.emojis.gekkoCoin} \`100\``,
+                }
+            );
+            return await interaction.reply({ embeds: [balanceError] });
+        }
+    }
+}
