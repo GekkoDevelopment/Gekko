7143c0d4b441dc056dbb9b655aa1f41730693d3d
kier2023 <kieronskelton.personal@gmail.com>
2024-05-17 09:49:22 +0100

    refactor v2 - deploy/delete cmds

--- delete-commands.js
+++ delete-commands.js
@@ -1,12 +1,10 @@
-import { REST, Routes } from 'discord.js';
-import config from './config.js';
+import config from './config.js.js';
+import REST from '@discordjs/rest';
+
 const rest = new REST().setToken(config.bot.token);
 
 // Global
-rest
-  .put(Routes.applicationCommands(config.bot.clientId), { body: [] })
-  .then(() => console.log("Successfully deleted all application commands."))
-  .catch(console.error);
+rest.put(Routes.applicationCommands(config.bot.clientId), { body: [] }).then(() => console.log('Successfully deleted all application commands.')).catch(console.error);
 
 // Guild
 // rest.put(Routes.applicationGuildCommands(config.bot.clientId, config.guilds.testGuild), { body: [] }).then(() => console.log('Successfully deleted all application guild commands.')).catch(console.error);
